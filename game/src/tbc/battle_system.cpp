47dd3b78530fd44ac28cac8ecc5e1f5f6b3a8ded
Onyx Leroy <antoinedupret6@gmail.com>
2024-09-26 13:41:46 +0200

    https://www.pokepedia.fr/Tutétékri

--- game/src/tbc/battle_system.cpp
+++ game/src/tbc/battle_system.cpp
@@ -1,22 +1,6 @@
 #include "battle_system.h"
 
 // ------------------------------------------------------------------------------------------------
-// UTILITY FUNCTION -------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------------------------
-
-bool compare_vectors_from_id(std::vector<BattleCharacter> v1, std::vector<BattleCharacter> v2, unsigned int id){
-    if (v1.size()!= v2.size()){ return false; }     // Vectors have different sizes
-    else if (v1.size() <= id){ return false; }      // Index is out of range
-    else{
-        bool res = true;
-        for (unsigned int i = id; i < v1.size(); i++){
-            if (v1[i] != v2[i]){ res = false; break; }
-        }
-        return res;
-    }
-}
-
-// ------------------------------------------------------------------------------------------------
 // CONST & DESTR ----------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------------
 
@@ -27,18 +11,27 @@ Battle_System *StartBattle(std::vector<BattleCharacter> playableCharacters, std:
     NewBattle->enemyCharacters = enemyCharacters;
     NewBattle->currentTurn = 0;
     NewBattle->maxTurnCount = MaxTurnCount;
-    NewBattle->currentCharacterIndex = 100;     // This can't be less than the maximum amount of characters
+    NewBattle->currentCharacterIndex = 100; // This can't be less than the maximum amount of characters
     NewBattle->isBattleOver = false;
     NewBattle->currentState = BattleState_Starting;
 
     std::vector<BattleCharacter> merged;
-    for (unsigned int i = 0; i < playableCharacters.size();i++){merged.push_back(playableCharacters[i]);}
-    for (unsigned int i = 0; i < enemyCharacters.size();i++){merged.push_back(enemyCharacters[i]);}
+    for (unsigned int i = 0; i < playableCharacters.size(); i++)
+    {
+        merged.push_back(playableCharacters[i]);
+    }
+    for (unsigned int i = 0; i < enemyCharacters.size(); i++)
+    {
+        merged.push_back(enemyCharacters[i]);
+    }
 
     NewBattle->currentCharacterOrder = merged;
     NewBattle->battlefield = merged;
 
-    if (DEBUG_MODE){ std::clog << "BattleDebug: a battle has been successfully launched." << std::endl; }
+    if (DEBUG_MODE)
+    {
+        std::clog << "BattleDebug: a battle has been successfully launched." << std::endl;
+    }
 
     return NewBattle;
 }
@@ -57,57 +50,80 @@ void DestroyBattle(Battle_System *Battle)
 void RunTurn(Battle_System *CurrentBattle)
 {
     BattleCharacter currentCharacter = CurrentBattle->currentCharacterOrder[CurrentBattle->currentCharacterIndex];
-    if (DEBUG_MODE) { std::clog << "BattleDebug: " << currentCharacter.GetName() << " is taking a turn." << std::endl; }
+    if (DEBUG_MODE)
+    {
+        std::clog << "BattleDebug: " << currentCharacter.GetName() << " is taking a turn." << std::endl;
+    }
 
-    if (currentCharacter.IsFriendly()){
+    if (currentCharacter.IsFriendly())
+    {
         // User turn code here
 
         // Player choose action code
-    } else {
+    }
+    else
+    {
         // Enemy turn code here
         // TODO: Implement enemy AI turn logic
     }
 }
 
-void RunBattleManager(Battle_System *CurrentBattle){
-    if (DEBUG_MODE){ std::clog << "BattleDebug: battle manager started." << std::endl; }
+void RunBattleManager(Battle_System *CurrentBattle)
+{
+    if (DEBUG_MODE)
+    {
+        std::clog << "BattleDebug: battle manager started." << std::endl;
+    }
 
-    switch (CurrentBattle->currentState){
-        case BattleState_Starting:
+    switch (CurrentBattle->currentState)
+    {
+    case BattleState_Starting:
+        CurrentBattle->currentState = BattleState_TurnStart;
+        break;
+    case BattleState_TurnStart:
+        CurrentBattle->currentTurn++;
+        if (CurrentBattle->currentTurn >= CurrentBattle->maxTurnCount)
+        {
+            if (DEBUG_MODE)
+            {
+                std::clog << "BattleDebug: battle ended after " << CurrentBattle->currentTurn << " turns (maximum exceeded)." << std::endl;
+            }
+            CurrentBattle->isBattleOver = true;
+            CurrentBattle->currentState = BattleState_TurnEnd;
+            break;
+        }
+        else
+        {
+            CurrentBattle->currentCharacterOrder = SortCharactersWRTStat(CurrentBattle->currentCharacterOrder);
+            CurrentBattle->currentCharacterIndex = 0;
+            CurrentBattle->currentState = BattleState_InATurn;
+            break;
+        }
+    case BattleState_InATurn:
+        CurrentBattle->currentCharacterIndex++;
+        if (CurrentBattle->currentCharacterIndex >= CurrentBattle->currentCharacterOrder.size())
+        {
+            CurrentBattle->currentState = BattleState_TurnEnd;
+            break;
+        }
+        else
+        {
+            RunTurn(CurrentBattle);
+            break;
+        }
+    case BattleState_TurnEnd:
+        if (CurrentBattle->isBattleOver)
+        {
+            CurrentBattle->currentState = BattleState_Ending;
+            break;
+        }
+        else
+        {
             CurrentBattle->currentState = BattleState_TurnStart;
             break;
-        case BattleState_TurnStart:
-            CurrentBattle->currentTurn++;
-            if (CurrentBattle->currentTurn >= CurrentBattle->maxTurnCount){
-                if (DEBUG_MODE){ std::clog << "BattleDebug: battle ended after " << CurrentBattle->currentTurn << " turns (maximum exceeded)." << std::endl; }
-                CurrentBattle->isBattleOver = true;
-                CurrentBattle->currentState = BattleState_TurnEnd;
-                break;
-            } else {
-                CurrentBattle->currentCharacterOrder = SortCharactersWRTStat(CurrentBattle->currentCharacterOrder);
-                CurrentBattle->currentCharacterIndex = 0;
-                CurrentBattle->currentState = BattleState_InATurn;
-                break;
-            }
-        case BattleState_InATurn:
-            CurrentBattle->currentCharacterIndex++;
-            if (CurrentBattle->currentCharacterIndex >= CurrentBattle->currentCharacterOrder.size()){
-                CurrentBattle->currentState = BattleState_TurnEnd;
-                break;
-            } else {
-                RunTurn(CurrentBattle);
-                break;
-            }
-        case BattleState_TurnEnd:
-            if (CurrentBattle->isBattleOver){
-                CurrentBattle->currentState = BattleState_Ending;
-                break;
-            } else {
-                CurrentBattle->currentState = BattleState_TurnStart;
-                break;
-            }
-        case BattleState_Ending:
-            // EndBattle(CurrentBattle);
-            break;
+        }
+    case BattleState_Ending:
+        // EndBattle(CurrentBattle);
+        break;
     }
 }
---
+++ game/src/tbc/components/priority_list.cpp
@@ -0,0 +1,136 @@
+#include "priority_list.hpp"
+
+// ------------------------------------------------------------------------------------------------
+
+PriorityList *CreateEmptyPriorityList()
+{
+    PriorityList *list = (PriorityList *)malloc(sizeof(PriorityList));
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
+    return list;
+}
+void AddToPriorityList(PriorityList *list, BattleCharacter *character, int relativeSpeed)
+{
+    switch (list->size)
+    {
+    case 0:
+        list->head = (PriorityEntity *)malloc(sizeof(PriorityEntity));
+        list->head->character = character;
+        list->head->relativeSpeed = relativeSpeed;
+        list->head->hasMadeAction = false;
+        list->head->next = NULL;
+        list->tail = list->head;
+        break;
+    case 1:
+        list->tail = (PriorityEntity *)malloc(sizeof(PriorityEntity));
+        list->tail->character = character;
+        list->tail->relativeSpeed = relativeSpeed;
+        list->tail->hasMadeAction = false;
+        list->tail->next = NULL;
+        list->head->next = list->tail;
+        break;
+    default:
+        PriorityEntity *newEntity = (PriorityEntity *)malloc(sizeof(PriorityEntity));
+        newEntity->character = character;
+        newEntity->relativeSpeed = relativeSpeed;
+        newEntity->hasMadeAction = false;
+        newEntity->next = NULL;
+        list->tail->next = newEntity;
+        list->tail = newEntity;
+        break;
+    }
+}
+void RemoveFromPriorityList(PriorityList *list, BattleCharacter *character)
+{
+    PriorityEntity *current = list->head;
+    PriorityEntity *previous = NULL;
+    while (current != NULL && current->character != character)
+    {
+        previous = current;
+        current = current->next;
+    }
+    if (current == NULL)
+    {
+        return;
+    }
+    if (previous == NULL)
+    {
+        list->head = current->next;
+    }
+    else
+    {
+        previous->next = current->next;
+    }
+    if (current == list->tail)
+    {
+        list->tail = previous;
+    }
+    free(current);
+    list->size--;
+}
+void DestroyPriorityList(PriorityList *list)
+{
+    PriorityEntity *current = list->head;
+    PriorityEntity *next;
+    while (current != NULL)
+    {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+    free(list);
+}
+
+// ------------------------------------------------------------------------------------------------
+
+void SortPriorityList(PriorityList *list)
+{
+    PriorityEntity *current, *min, *previous;
+    int minIndex;
+
+    for (current = list->head; current != NULL; current = current->next)
+    {
+        min = current;
+        minIndex = 0;
+        for (int i = 1; i < list->size; i++)
+        {
+            if (list->head->relativeSpeed < min->relativeSpeed)
+            {
+                min = list->head;
+                minIndex = i;
+            }
+        }
+        if (minIndex != 0)
+        {
+            previous = min;
+            for (int i = minIndex - 1; i >= 0; i--)
+            {
+                previous = previous->next;
+            }
+            previous->next = min->next;
+            min->next = list->head;
+            list->head = min;
+        }
+    }
+}
+void CleanPreviousActions(PriorityList *list)
+{
+    std::vector<BattleCharacter *> charactersToRemove;
+    PriorityEntity *current = list->head;
+    while (current != NULL)
+    {
+        if (current->hasMadeAction)
+        {
+            charactersToRemove.push_back(current->character);
+        }
+        current = current->next;
+    }
+
+    for (unsigned int i = 0; i < charactersToRemove.size(); i++)
+    {
+        RemoveFromPriorityList(list, charactersToRemove[i]);
+    }
+}
+
+// ------------------------------------------------------------------------------------------------
